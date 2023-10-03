5c73b4e8893101c98c54ce1433d5009c60594cc1
Belqisivana <belqisivana7@gmail.com>
2023-10-03 14:15:20 +0700

    Create SistemKasir.java

---
+++ SistemKasir.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SistemKasir {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String member;
+        double diskon, bayar, total;
+        System.out.println("Apalah anda punya kartu member (y atau t)? :");
+        member = input.nextLine();
+        System.out.println("Berapa total harga belanjaan anda ? Rp ");
+        total = input.nextDouble();
+        if (member.equals("y")) {
+            if (total >= 200000) {
+                diskon = total * 0.15;
+            } else {
+                diskon = total * 0.10;
+            }
+            bayar = total - diskon;
+            System.out.println("Diskon: Rp " + diskon);
+            System.out.println("Total yang harus dibayar: Rp " + bayar);
+        }
+        if (member.equals("t")) {
+            if (total >= 300000) {
+                diskon = total * 0.5;
+            } else
+                System.out.println("GAK OLEH DISKON TOL");
+        }
+
+    }
+}
