d0231fff49a4b3e3741f33fb5b4351800e697409
Belqisivana <144077213+Belqisivana@users.noreply.github.com>
2023-10-09 10:46:15 +0700

    Update SistemKasir.java

--- SistemKasir.java
+++ SistemKasir.java
@@ -3,13 +3,28 @@ import java.util.Scanner;
 public class SistemKasir {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String member;
+        String member, namaBarang, beliLagi;
+        int hargaBarang, jmlBarang, totalBayar = 0;
         double diskon, bayar, total;
-        System.out.println("Apalah anda punya kartu member (y atau t)? :");
+
+        do {
+            System.out.println("Masukkan nama barang: ");
+            namaBarang = input.next();
+            System.out.println("Masukkan harga barang: ");
+            hargaBarang = input.nextInt();
+            System.out.println("Masukkan jumlah barang: ");
+            jmlBarang = input.nextInt();
+            System.out.println("Beli lagi (y/n)?");
+            beliLagi = input.next();
+            total = hargaBarang * jmlBarang;
+            totalBayar += total;
+        } while (beliLagi.equals("y"));
+
+        System.out.println("Total harga belanjaan anda Rp " + totalBayar);
+        input.nextLine();
+        System.out.println("Apakah anda punya kartu member (ya atau tidak)? :");
         member = input.nextLine();
-        System.out.println("Berapa total harga belanjaan anda ? Rp ");
-        total = input.nextDouble();
-        if (member.equals("y")) {
+        if (member.equals("ya")) {
             if (total >= 200000) {
                 diskon = total * 0.15;
             } else {
@@ -18,12 +33,15 @@ public class SistemKasir {
             bayar = total - diskon;
             System.out.println("Diskon: Rp " + diskon);
             System.out.println("Total yang harus dibayar: Rp " + bayar);
-        }
-        if (member.equals("t")) {
+        } else {
             if (total >= 300000) {
-                diskon = total * 0.5;
+                diskon = total * 0.05;
+                bayar = total - diskon;
+                System.out.println("Hasil diskon " + diskon);
+                System.out.println("Total yang harus dibayar: Rp " + bayar);
             } else
                 System.out.println("Tidak dapat diskon");
+
         }
 
     }
